cfecd2943dc8fadda556055455d7e5580284e64f
plokamar1 <p.dorgiak@gmail.com>
2017-01-12 17:27:25 +0200

    object test

--- js/dataget.js
+++ js/dataget.js
@@ -4,8 +4,8 @@ app1.controller("ctrl1", function($scope){
     
     $scope.onClick = function(){
         $.getJSON('http://www.omdbapi.com/?r=json&s='+$scope.title, function(data){
-            console.log(data);
+            console.log(Object.keys(data.Search[0]));
+            $scope.datalist = data.Search[0];
         });
-
     }
 });
