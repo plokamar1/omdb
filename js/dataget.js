e642de75aae93df9ea48bcac071744b5fde7b814
plokamar1 <p.dorgiak@gmail.com>
2017-01-12 12:56:30 +0200

    Added a function that is called by the search button. Also i get the data from the api with .getJSON

--- js/dataget.js
+++ js/dataget.js
@@ -2,4 +2,11 @@ var app1 = angular.module("app1",[]);
 
 app1.controller("ctrl1", function($scope){
     
+    $scope.onClick = function(){
+        $.getJSON('http://www.omdbapi.com/?r=json&t='+$scope.title, function(data){
+           console.log(data);
+
+        });
+
+    }
 });
