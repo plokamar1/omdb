8d3de9cc6e4686c5ef21692a81af408a084ba26b
plokamar1 <p.dorgiak@gmail.com>
2017-01-12 14:29:41 +0200

    By changing the url variable t to s we get multiple results for one search.

--- js/dataget.js
+++ js/dataget.js
@@ -3,9 +3,8 @@ var app1 = angular.module("app1",[]);
 app1.controller("ctrl1", function($scope){
     
     $scope.onClick = function(){
-        $.getJSON('http://www.omdbapi.com/?r=json&t='+$scope.title, function(data){
-           console.log(data);
-
+        $.getJSON('http://www.omdbapi.com/?r=json&s='+$scope.title, function(data){
+            console.log(data);
         });
 
     }
