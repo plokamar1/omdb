bd55c237e789700f8469cc1b9b565fda1cc93ce5
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-01-12 21:00:53 +0200

    If the title doesnt change then it doesnt request more. If there is a response then pass the data.

--- js/dataget.js
+++ js/dataget.js
@@ -1,11 +1,25 @@
 var app1 = angular.module("app1",[]);
-
+var titleBef;
+//The variable titleBef saves the state of the user's search when pressing the button so as
+//if he presses again the search button and he hasnt changed the string then the request
+//wont happen again
 app1.controller("ctrl1", function($scope){
-    
     $scope.onClick = function(){
-        $.getJSON('http://www.omdbapi.com/?r=json&s='+$scope.title, function(data){
-            console.log(Object.keys(data.Search[0]));
-            $scope.datalist = data.Search[0];
-        });
+        
+        if(titleBef != $scope.title){
+
+            $.getJSON('http://www.omdbapi.com/?&type=movie&r=json&s='+$scope.title, function(data){
+            console.log(data.Search);
+            console.log(data.Response);
+            if(data.Response = "True"){
+                titleBef = $scope.title;
+                $scope.movies = data.Search;
+            }else{
+                $scope.movies = 0;
+            }
+
+            });  
+        }
     }
+
 });
