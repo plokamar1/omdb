7e954e38be0ad6bfe6ed0d0c472066dc72cea90a
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-01-12 21:11:55 +0200

    Indentation

--- js/dataget.js
+++ js/dataget.js
@@ -1,24 +1,25 @@
-var app1 = angular.module("app1",[]);
+var app1 = angular.module("app1", []);
 var titleBef;
 //The variable titleBef saves the state of the user's search when pressing the button so as
 //if he presses again the search button and he hasnt changed the string then the request
 //wont happen again
-app1.controller("ctrl1", function($scope){
-    $scope.onClick = function(){
-        
-        if(titleBef != $scope.title){
 
-            $.getJSON('http://www.omdbapi.com/?&type=movie&r=json&s='+$scope.title, function(data){
-            console.log(data.Search);
-            console.log(data.Response);
-            if(data.Response = "True"){
-                titleBef = $scope.title;
-                $scope.movies = data.Search;
-            }else{
-                $scope.movies = 0;
-            }
+app1.controller("ctrl1", function ($scope) {
+    $scope.onClick = function () {
 
-            });  
+        if (titleBef != $scope.title) {
+
+            $.getJSON('http://www.omdbapi.com/?&type=movie&r=json&s=' + $scope.title, function (data) {
+                //console.log(data.Search);
+                console.log(data.Response);
+                if (data.Response = "True") {
+                    titleBef = $scope.title;
+                    $scope.movies = data.Search;
+                } else {
+                    $scope.movies = 0;
+                }
+
+            });
         }
     }
 
